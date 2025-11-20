d1b509e2aa6ebafe694478423273807fc75c072e
Hazar1652 <vasilevicn183@gmail.com>
2025-11-20 13:45:20 +0200

    lesson7_hw1

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,8 +1,11 @@
 import {type FC, memo} from "react";
 //огортаємо вивід 'user' в функцію memo для того щоб елемен не переренджувався також передаємо в середину функцію foo для прикоаду
-export const UserComponent:FC<{foo:() => void }> = memo( () => {
+//передаємо також arr для прикладу використання useMemo
+export const UserComponent:FC<{foo:() => void, arr: number[] }> = memo( ({arr}) => {
     //Виводимо 'User'
-    console.log('User')
+    console.log('User');
+    //Виводимо arr
+    console.log(arr);
     return (
         <div>user</div>
     );
--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,32 +1,36 @@
 import {UserComponent} from "./UserComponent.tsx";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 
 export const UsersComponent = () => {
     //виводимо в консоль "test"
     console.log('users')
     //створюємо useState для юзерів
     const [, setUsers] = useState([])
+    //створюємо масив і застосуваємо useMemo для того щоб не переренджувався масив
+    const arr: number[] = useMemo(() => {
+        return [11, 22, 33]
+    }, []);
     //     створюємо зовнішню функцію foo яку будемо передавати в fc component
-    const foo = useCallback(() =>{
+    const foo = useCallback(() => {
         console.log('test')
-    },[]);
+    }, []);
     //створюємо useEffect
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(value => value.json())
-        .then(value => {
-            //передаємо value які отримали
-            setUsers(value);
+            .then(value => value.json())
+            .then(value => {
+                //передаємо value які отримали
+                setUsers(value);
 
-        });
+            });
         return () => {
             console.log('unsubscribe');
         }
     }, []);
     return (
         <div> users component
-            {/*передаємо fc foo*/}
-            <UserComponent foo={foo}/>
+            {/*передаємо fc foo і аrr*/}
+            <UserComponent foo={foo} arr={arr}/>
         </div>
     );
 };
