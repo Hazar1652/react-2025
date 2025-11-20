f6889b86d3f4177ab1b8ea2f6cf52e6556857a4d
Hazar1652 <vasilevicn183@gmail.com>
2025-11-20 13:32:16 +0200

    lesson7_hw1

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,5 +1,5 @@
 import {type FC, memo} from "react";
-//огортаємо вивід 'user' в функцію memo для того щоб елемен не переренджувався
+//огортаємо вивід 'user' в функцію memo для того щоб елемен не переренджувався також передаємо в середину функцію foo для прикоаду
 export const UserComponent:FC<{foo:() => void }> = memo( () => {
     //Виводимо 'User'
     console.log('User')
